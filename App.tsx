7210348bcfb1a55b96cdc00ee252245d8bee17d1
Javier Carrizo <carrizo38@gmail.com>
2025-05-11 13:58:21 -0300

    Add App.tsx 1746982698027

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import TabNavigator from './src/navigation/TabNavigator';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
